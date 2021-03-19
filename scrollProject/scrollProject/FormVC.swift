baff81abb45f4cb53dac392d55fc02d6122ee09a
Gina Rubira <grubirapa@gmail.com>
2021-03-19 13:58:55 +0100

    Comprovació DNI completada

--- scrollProject/scrollProject/FormVC.swift
+++ scrollProject/scrollProject/FormVC.swift
@@ -14,9 +14,45 @@ class FormVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func textFieldDNI(_ sender: Any) {
-       
+    @IBAction func textFieldDNI(_ sender: UITextField) {
+        if let text = sender.text {
+            registerButton.isEnabled = isValidID(text)
+        }
     }
+    
+    let abc = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+    
+    func isValidID (_ documentID: String) -> Bool {
+        guard let lastCharacter = documentID.last else { return false }
+        if documentID.count == 9 && lastCharacter.isLetter {
+            var documentID = documentID
+            // Ho convertim a variable ja que els paràmetres són constants i volem variar el seu valor per treure-li la última lletra perque només ens quedin números i puguem passar el String a Int.
+            documentID.removeLast()
+            guard let num = Int(documentID) else { return false }
+            let numResto = num % 23
+            if String(lastCharacter) == abc[numResto] {
+                return true
+            }
+        } else {
+            return false
+        }
+        return false
+    }
+    
+    
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        guard let lastLetter = textField.text?.last else { return }
+//
+//        if textField.text?.count == 9 && lastLetter.isLetter {
+//            registerButton.isEnabled = true
+//        } else {
+//            registerButton.isEnabled = false
+//        }
+////        if textField.text != "" {
+////            print(textField.text ?? "hey")
+////        }
+//    }
     
     @IBOutlet var registerButton: UIButton!
     
@@ -25,7 +61,7 @@ class FormVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        registerButton.isEnabled = false
     }
 
 
