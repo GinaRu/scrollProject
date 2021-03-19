8869371503ea736dd6c9a0e82869673b7e931500
Gina Rubira <grubirapa@gmail.com>
2021-03-19 12:09:05 +0100

    scroll muntat

---
+++ scrollProject/scrollProject/FormVC.swift
@@ -0,0 +1,32 @@
+//
+//  FormVC.swift
+//  scrollProject
+//
+//  Created by Georgina Rubira Pairó on 19/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class FormVC: UIViewController {
+
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func textFieldDNI(_ sender: Any) {
+       
+    }
+    
+    @IBOutlet var registerButton: UIButton!
+    
+    @IBAction func registerTouched(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
---
+++ scrollProject/scrollProject/MapVC.swift
@@ -0,0 +1,8 @@
+//
+//  MapVC.swift
+//  scrollProject
+//
+//  Created by Georgina Rubira Pairó on 19/03/2021.
+//
+
+import Foundation
