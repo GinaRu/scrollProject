df81b672770823bbd922f36267b91547e9c722ce
Gina Rubira <grubirapa@gmail.com>
2021-03-24 10:58:31 +0100

    Polyline creada i segueix correctament l'usuari.

--- scrollProject/scrollProject/MapaVC.swift
+++ scrollProject/scrollProject/MapaVC.swift
@@ -14,17 +14,17 @@ class MapaVC: UIViewController {
     @IBOutlet var mapaView: MKMapView!
     
     let locationManager = CLLocationManager()
+    var userCoordinates = [CLLocation]()
+    var currentPolyline: MKPolyline? = nil
+    var isFollowingUserLocation: Bool = true
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        mapaView.delegate = self
         
         checkLocationServices()
-        
-
     }
     
     func checkLocationServices () {
@@ -52,6 +52,23 @@ class MapaVC: UIViewController {
             break
         }
     }
+    
+    func updatePolyline() {
+//        var coordinates: [CLLocationCoordinate2D]
+//        for userLocation in userCoordinates {
+//            coordinates.append(userLocation.coordinate)
+//        }
+        // Lo mismo que:
+        let coordinates: [CLLocationCoordinate2D] = userCoordinates.map{$0.coordinate}
+        let newPolyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        mapaView.addOverlay(newPolyline)
+        
+        if let currentPolyline = currentPolyline {
+            mapaView.removeOverlay(currentPolyline)
+        }
+        currentPolyline = newPolyline
+
+    }
 }
 
 extension MapaVC: CLLocationManagerDelegate {
@@ -60,18 +77,28 @@ extension MapaVC: CLLocationManagerDelegate {
         if let location = locations.last {
 
             let region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+                userCoordinates.append(location)
+                updatePolyline()
                 mapaView.setRegion(region, animated: true)
             }
-        
-
-
     }
 
-    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         locationManager.stopUpdatingLocation()
         print("HI HA HAGUT UN ERROR: \(error)")
     }
     
     
+}
+
+extension MapaVC: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let routePolyline = overlay as? MKPolyline {
+                let renderer = MKPolylineRenderer(polyline: routePolyline)
+                renderer.strokeColor = UIColor.magenta.withAlphaComponent(0.8)
+                renderer.lineWidth = 7
+                return renderer
+            }
+            return MKOverlayRenderer()
+        }
 }
