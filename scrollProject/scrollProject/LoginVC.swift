0c3c4da19ba9c3141833f6cd068ee099b92ca83c
Gina Rubira <grubirapa@gmail.com>
2021-03-24 14:03:26 +0100

    Guardem el nom d'usuari i la contrassenya al loginVC amb keychain.

--- scrollProject/scrollProject/LoginVC.swift
+++ scrollProject/scrollProject/LoginVC.swift
@@ -12,6 +12,11 @@ class LoginVC: UIViewController {
     @IBOutlet var userTextField: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        saveUsernameAndPassword()
+    }
+    
     @objc func handleTap(_ sender: UITapGestureRecognizer) {
 //        userTextField.resignFirstResponder()
 //        passwordTextfield.resignFirstResponder()
@@ -19,16 +24,33 @@ class LoginVC: UIViewController {
         self.view.endEditing(true)
     }
     
-    func hideKeyboardWhenTap() {
+    func hideKeyboardWhenTapView() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         self.view.addGestureRecognizer(tap)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        hideKeyboardWhenTap()
+    func saveUsernameAndPassword() {
+        if let username = userTextField.text {
+            KeychainService.removeString(service: "service username", account: "account username")
+            KeychainService.saveString(service: "service username", account: "account username", data: username)
+        }
+        if let password = passwordTextfield.text {
+            KeychainService.removeString(service: "service password", account: "account password")
+            KeychainService.saveString(service: "service password", account: "account password", data: password)
+        }
+    }
+    
+    func LoadUsernameAndPassword () {
+        let password = KeychainService.loadString(service: "service password", account: "account password")
+        let username = KeychainService.loadString(service: "service username", account: "account username")
+        userTextField.text = username
+        passwordTextfield.text = password
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideKeyboardWhenTapView()
+        LoadUsernameAndPassword()
+    }
 }
 
