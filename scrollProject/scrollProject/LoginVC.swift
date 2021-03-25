6669ed1a5420706c2ef25acb7beffffc61e3ef4d
Gina Rubira <grubirapa@gmail.com>
2021-03-25 12:42:27 +0100

    El botó i la seva animació s'adapten al teclat.

--- scrollProject/scrollProject/LoginVC.swift
+++ scrollProject/scrollProject/LoginVC.swift
@@ -11,7 +11,7 @@ class LoginVC: UIViewController {
 
     @IBOutlet var userTextField: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
-    
+    @IBOutlet var constraintLogin: NSLayoutConstraint!
     
     @IBAction func loginTapped(_ sender: UIButton) {
         saveUsernameAndPassword()
@@ -51,6 +51,67 @@ class LoginVC: UIViewController {
         super.viewDidLoad()
         hideKeyboardWhenTapView()
         LoadUsernameAndPassword()
+        registerKeyboardNotifications()
+        
+    }
+    deinit {
+        removeKeyboardNotifications()
+    }
+}
+
+extension LoginVC {
+    
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+
+                var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+                keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+            
+            let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber
+                    let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as! NSNumber
+                    let animationCurve = UInt(exactly: curve) ?? 0
+                    let durationInterval = TimeInterval(exactly: duration) ?? 0.5
+        
+                    UIView.animate(withDuration: durationInterval,
+                                   delay: 0,
+                                   options: [UIView.AnimationOptions(rawValue: animationCurve)],
+                                   animations: {
+                                    self.constraintLogin.constant = keyboardFrame.size.height + 8
+                                    self.view.layoutIfNeeded()
+                    }, completion: nil)
+        
+
+        print("EL TECLADO APARECE: \(keyboardFrame.size.height)")
+
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else { return }
+        let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber
+                let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as! NSNumber
+                let animationCurve = UInt(exactly: curve) ?? 0
+                let durationInterval = TimeInterval(exactly: duration) ?? 0.5
+    
+                UIView.animate(withDuration: durationInterval,
+                               delay: 0,
+                               options: [UIView.AnimationOptions(rawValue: animationCurve)],
+                               animations: {
+                                self.constraintLogin.constant = 40
+                                self.view.layoutIfNeeded()
+                }, completion: nil)
+        
+
+        print("EL TECLADO DESAPARECE")
+    }
+
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
